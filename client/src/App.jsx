3aa2f3bfff8abe46ee27aa2f1294b379eb3e59ef
Malik Washington <malikwashington@gmail.com>
2025-02-19 14:46:20 -0500

    connected the backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout";
+
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} />
+  ));
 
   return (
     <>
+    <h2>It's A Smorgasborg</h2>
     </>
   )
 }
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <div>Welcome To The Home Pag</div>
+    </main>
+  )
+}
+
+export default Home
